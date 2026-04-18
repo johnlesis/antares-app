9f4a09ffad8865e5d19002b26d614221dd31f3e0
Johnlesis <johnlesis91@gmail.com>
2026-04-18 19:27:15 +0300

    fix: remobed dotenv from index.php and added to Application boot

--- public/index.php
+++ public/index.php
@@ -5,9 +5,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-
 use Antares\Application;
 use App\Middleware\CorsMiddleware;
 use App\Providers\AppServiceProvider;
